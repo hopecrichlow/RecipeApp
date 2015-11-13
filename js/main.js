829f9674c9bb29dac1d7771cdf5130f00efc3e7e
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-11-12 20:51:55 -0500

    Files and HTML set-up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
+import angular from 'angular';
+
+//Creating a Module
+angular.module('taco', []);
+
+//Referencing a Module
+angular.module('taco');
+
 
 console.log('Hello, World');
